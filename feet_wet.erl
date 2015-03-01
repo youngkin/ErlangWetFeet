29857b3ea4f3018442718ce159f7b0b5a4c635ba
Rich Youngkin <rich.youngkin@gmail.com>
2015-03-01 09:20:56 -0600

    Added comments

--- feet_wet.erl
+++ feet_wet.erl
@@ -2,6 +2,48 @@
 %% This is also a comment. Use for function comments and/or section comments(e.g., API).
 % So is this, use for statement comments
 
+%%% -------------------------------------------------------------------------------------
+%%%
+%%% This is module documentation for feet_wet.
+%%%
+%%% Here is a sample Erlang shell session with the module.
+%%%
+%%%    DNVCOML-32JFD59:ErlangWetFeet uyounri$ erl
+%%%    Erlang/OTP 17 [erts-6.1] [source] [64-bit] [smp:8:8] [async-threads:10] [hipe] [kernel-poll:false] [dtrace]
+%%% 
+%%%    Eshell V6.1  (abort with ^G)
+%%%    1> c(feet_wet).
+%%%    {ok,feet_wet}
+%%%    2> feet_wet:hello_world().
+%%%    Hello World!
+%%%    "Hello World!"
+%%%    3> feet_wet:echo("ECHO, ECHo, ECho, Echo, echo...").
+%%%    "ECHO, ECHo, ECho, Echo, echo..."
+%%%    4> feet_wet:sum(2).
+%%%    3
+%%%    5> FeetWetPid = spawn(feet_wet, loop, [0]).
+%%%    <0.42.0>
+%%%    6> FeetWetPid ! {self(), "Hello"}.
+%%%    {<0.32.0>,"Hello"}
+%%%    7> flush().
+%%%    Shell got "Hello World!"
+%%%    ok
+%%%    8>
+%%%
+%%% Line 1 compiles the feet_wet module.
+%%% Line 2 invokes the hello_world function.
+%%% Line 3 likewise calls echo.
+%%% Line 4 likewise calls sum.
+%%% Line 5 is more interesting, it spawns a process that is a feet_wet service process.
+%%% Line 6 sends a message {self(), "Hello"} to the feet_wet service process.
+%%% Line 7 flushes the message sent back to the calling process (the shell).
+%%%
+%%% Fred Hebert's "Learn You Some Erlang for Great Good" is a great resource for learning 
+%%% Erlang as is Joe Armstrong's "Erlang programming". There are many other resources that
+%%% can be found by a simple google search on "Erlang".
+%%%
+%%% -------------------------------------------------------------------------------------
+
 -module(feet_wet). % Defines a module named feet_wet
 -author("uyounri"). % Part of a set of module info attributes
 
@@ -47,7 +89,7 @@ match_string(Match) ->
 
 %%
 %% loop demonstrates how to implement a service in Erlang. Services run  in an Erlang process that can be created by:
-%%    FeetWet = spawn(feet_wet, loop, [0]).
+%%    FeetWetPid = spawn(feet_wet, loop, [0]).
 %%
 %% There are other ways to create processes (e.g., spawn_link/3). The arguments of "spawn*" demonstrate a common 
 %% pattern in Erlang, MFA, aka Module, Function, Arguments. In this case "M" is "feet_wet", "F" is looop, and
@@ -63,12 +105,27 @@ match_string(Match) ->
 %% appropriate. Another option is to have a specific "stop" message and continue processing if an unexpected message
 %% is encountered. This is a good practice since unmatched messages stay in a process's mailbox.
 %%
+%% Messages can be sent to this process as follows (using the Pid (e.g., FeetWetPid) from the spawn call above:
+%%    FeetWetPid ! {self(), "Hello"}.
+%%    FeetWetPid ! {self(), "Echo"}.
+%%    FeetWetPid ! {self(), {sum_numbers, some_arbitrary_number_such_as_2}.
+%%        FeetWetPid ! {self(), {sum_numbers, 2}}.
+%%    FeetWetPid ! {stop}.
+%%
+%% To see the results of sending the message type "flush()." at the shell command line.
+%%
+%% The Erlang OTP framework provides a robust way to implement Erlang services (e.g, gen_server) so that all the
+%% receive loop boiler plate can be skipped. It also automatically provides many other capabilities that would 
+%% be difficult to write by hand.
+%%
+%%
 loop(_State) ->
   receive
     {Pid, Anything} ->
-      Pid ! match_string(Anything),
-      loop(_State);
-    _ -> exit(normal)
+      Pid ! match_string(Anything), % this expression sends the response [the result from match_string(Anything) ]
+                                    %  message back to the calling process
+      loop(_State); % continues the loop
+    _ -> exit(normal) % exits the loop if any unmatched message is sent to the process.
   end.
 
 %%
